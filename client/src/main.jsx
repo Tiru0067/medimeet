334b223e37c13faae43c10ed14b08ee2ebf46602
tiru0067 <tiru0067@gmail.com>
2025-08-03 20:45:51 +0530

    updated vite.config.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/medimeet">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
